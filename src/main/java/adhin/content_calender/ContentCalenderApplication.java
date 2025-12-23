7567d811c7b041e5d070d063541b36a626d20a39
Adhinmahesh <adhinmahesh6@gmail.com>
2025-12-23 12:00:02 +0530

    change in application property and crearte a new file dataloader

--- src/main/java/adhin/content_calender/ContentCalenderApplication.java
+++ src/main/java/adhin/content_calender/ContentCalenderApplication.java
@@ -1,7 +1,15 @@
 package adhin.content_calender;
 
+import adhin.content_calender.model.Content;
+import adhin.content_calender.model.Status;
+import adhin.content_calender.model.Type;
+import adhin.content_calender.repository.ContentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class ContentCalenderApplication {
@@ -10,5 +18,25 @@ public class ContentCalenderApplication {
 
         SpringApplication.run(ContentCalenderApplication.class, args);
 	}
+
+    @Bean
+
+    CommandLineRunner commandLineRunner(ContentRepository repository){
+        return args ->{
+            Content content= new Content(  null,
+                    "hello world ",
+                    "all about me ",
+                    Status.IDEA,
+                    Type.VIDEO,
+                    LocalDateTime.now(),
+                    null,
+                    ""
+            );
+            repository.save(content);
+
+            // we can insert some data in to the data base
+
+        };
+    }
 
 }
